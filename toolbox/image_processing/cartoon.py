dae1ca3c7ea042a174369356b4069d08baf7fe99
Isaac Getto <isaac.getto@gmail.com>
2015-03-29 23:19:47 -0400

    draw eyes

--- toolbox/image_processing/cartoon.py
+++ toolbox/image_processing/cartoon.py
@@ -2,7 +2,7 @@ import cv2
 
 def draw_face(frame, x, y, w, h):
     draw_outline(frame, x, y, w, h)
-    # draw_eyes(x, y, w, h)
+    draw_eyes(frame, x, y, w, h)
     # draw_nose(x, y, w, h)
     # draw_mouth(x, y, w, h)
     # draw_hair(x, y, w, h)
@@ -13,3 +13,13 @@ def draw_outline(frame, x, y, w, h):
     color = (0, 0, 255)
     thickness = 5
     cv2.circle(frame, center, radius, color, thickness)
+
+def draw_eyes(frame, x, y, w, h):
+    center1 = (x + w / 3, y + h / 3)
+    center2 = (x + 2 * w / 3, y + h / 3)
+    radius = min(w, h) / 10
+    color = (255, 0, 0)
+    thickness = 4
+        
+    cv2.circle(frame, center1, radius, color, thickness)
+    cv2.circle(frame, center2, radius, color, thickness)
