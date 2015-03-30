24b599043cb2311ec489f154edb92ae48a581c72
Isaac Getto <isaac.getto@gmail.com>
2015-03-29 23:48:27 -0400

    draw hair

--- toolbox/image_processing/cartoon.py
+++ toolbox/image_processing/cartoon.py
@@ -5,7 +5,7 @@ def draw_face(frame, x, y, w, h):
     draw_eyes(frame, x, y, w, h)
     draw_nose(frame, x, y, w, h)
     draw_mouth(frame, x, y, w, h)
-    # draw_hair(x, y, w, h)
+    draw_hair(frame, x, y, w, h)
 
 def draw_outline(frame, x, y, w, h):
     center = (x + w/ 2, y + h/ 2)
@@ -29,10 +29,11 @@ def draw_nose(frame, x, y, w, h):
     nose_bot_left = (int(x + (w / 2) - (0.1 * w)),  y + 2 * w / 3)
     nose_bot_right = (int(x + (w / 2) + (0.1 * w)),  y + 2 * w / 3)
     color = (0, 255, 0)
+    thickness = 4
     
-    cv2.line(frame, nose_top, nose_bot_left, color)
-    cv2.line(frame, nose_top, nose_bot_right, color)
-    cv2.line(frame, nose_bot_left, nose_bot_right, color)
+    cv2.line(frame, nose_top, nose_bot_left, color, thickness)
+    cv2.line(frame, nose_top, nose_bot_right, color, thickness)
+    cv2.line(frame, nose_bot_left, nose_bot_right, color, thickness)
 
 def draw_mouth(frame, x, y, w, h):
     center = (x + w / 2, y + 4 * h / 5) 
@@ -45,3 +46,15 @@ def draw_mouth(frame, x, y, w, h):
     thickness = 10
 
     cv2.ellipse(frame, center, (width, height), angle, startAngle, endAngle, color, thickness)
+
+def draw_hair(frame, x, y, w, h):
+    center = (int(x + w/ 2), int(y + h/ 2))
+    radius = min(w, h) / 2
+    radius = int(radius * 1.1)
+    angle = 0
+    startAngle = 220
+    endAngle = 320
+    color = (60, 100, 160)
+    thickness = 20
+
+    cv2.ellipse(frame, center, (radius, radius), angle, startAngle, endAngle, color, thickness)
