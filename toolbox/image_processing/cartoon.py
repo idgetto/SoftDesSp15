ccba4f0116485e6771ea9ce50a186f7de50202ba
Isaac Getto <isaac.getto@gmail.com>
2015-03-29 23:30:52 -0400

    draw nose

--- toolbox/image_processing/cartoon.py
+++ toolbox/image_processing/cartoon.py
@@ -3,7 +3,7 @@ import cv2
 def draw_face(frame, x, y, w, h):
     draw_outline(frame, x, y, w, h)
     draw_eyes(frame, x, y, w, h)
-    # draw_nose(x, y, w, h)
+    draw_nose(frame, x, y, w, h)
     # draw_mouth(x, y, w, h)
     # draw_hair(x, y, w, h)
 
@@ -23,3 +23,13 @@ def draw_eyes(frame, x, y, w, h):
         
     cv2.circle(frame, center1, radius, color, thickness)
     cv2.circle(frame, center2, radius, color, thickness)
+
+def draw_nose(frame, x, y, w, h):
+    nose_top = (x + w / 2, y + 2 * w / 5)
+    nose_bot_left = (int(x + (w / 2) - (0.1 * w)),  y + 2 * w / 3)
+    nose_bot_right = (int(x + (w / 2) + (0.1 * w)),  y + 2 * w / 3)
+    color = (0, 255, 0)
+    
+    cv2.line(frame, nose_top, nose_bot_left, color)
+    cv2.line(frame, nose_top, nose_bot_right, color)
+    cv2.line(frame, nose_bot_left, nose_bot_right, color)
